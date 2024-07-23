d2d3ac50b6b895a020179d78dfcce1adaea96bea
Steve Taylor <steve@p1smt.com>
2024-07-23 14:59:29 +0000

    Add my details to db of progrqmmers

--- sql-crud.py
+++ sql-crud.py
@@ -80,16 +80,25 @@ tim_berners_lee = Programmer(
     famous_for="World Wide Web"
 )
 
+steve_taylor = Programmer(
+    first_name="Steve",
+    last_name="Taylor",
+    gender="M",
+    nationality="British",
+    famous_for="Nothing"
+)
+
 # add each instance of our programmers to our session
-session.add(ada_lovelace)
-session.add(alan_turing)
-session.add(grace_hopper)
-session.add(margaret_hamilton)
-session.add(bill_gates)
-session.add(tim_berners_lee)
+# session.add(ada_lovelace)
+# session.add(alan_turing)
+# session.add(grace_hopper)
+# session.add(margaret_hamilton)
+# session.add(bill_gates)
+# session.add(tim_berners_lee)
+# session.add(steve_taylor)
 
 # commit our session to the database
-session.commit()
+# session.commit()
 
 
 # query the database to find all Programmers
